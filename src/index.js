d18e2830290658ea6a5c5fc0e0517cc5e67ed989
Zach <zawood11@gmail.com>
2021-05-22 15:04:35 -0600

    fetchdata and renderdata

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("file loaded");
+function fetchTeams() {
+    fetch("https://free-nba.p.rapidapi.com/teams", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "eea0f00af2msh1d7263f0e338362p1c74e0jsn3f1c0b5b1925",
+		"x-rapidapi-host": "free-nba.p.rapidapi.com"
+	}
+})
+.then(res => res.json())
+.then(teams => {
+    // const teamData = teams.data
+    const teamData = Object.values(teams.data)
+    console.log(Object.values(teams.data))
+    console.log(teamData[7].abbreviation)
+    teamData.forEach(renderTeam)
+})
+.catch(err => {
+	console.error(err);
+        }
+    )}
+
+function renderTeam(teams) {
+
+    const teamContainer = document.getElementById("west-list")
+    const liTag = document.createElement('li')
+    const pTag = document.createElement('p')
+
+    pTag.innerText = 'team'
+    liTag.append(pTag)
+
+    teamContainer.appendChild(liTag)
+}
+
+fetchTeams();
